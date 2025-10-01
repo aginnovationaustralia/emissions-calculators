053d8523210d0eb0a44d670fa21acae8e39c16ae
Eddie Sholl <eddie.sholl@exogee.com>
2025-10-01 20:44:31 +1000

    Upgrade syntax in example ts project

--- examples/ts-client/src/beef/v300.ts
+++ examples/ts-client/src/beef/v300.ts
@@ -1,7 +1,14 @@
-import { calculateBeef } from '@aginnovationaustralia/emissions-calculators/versions/3.0.0/calculators';
-import { BeefInput } from '@aginnovationaustralia/emissions-calculators/versions/3.0.0/types/Beef/input';
+import { v3_0_0 } from '@aginnovationaustralia/emissions-calculators';
 import { beefInputData } from './input';
+const { calculateBeef } = v3_0_0.Calculators;
+const { BeefInput } = v3_0_0.Types;
 
-const beefInput300: BeefInput = beefInputData;
+const beefInput300 = new BeefInput();
+beefInput300.beef = beefInputData.beef;
+beefInput300.state = beefInputData.state;
+beefInput300.northOfTropicOfCapricorn = beefInputData.northOfTropicOfCapricorn;
+beefInput300.rainfallAbove600 = beefInputData.rainfallAbove600;
+beefInput300.burning = beefInputData.burning;
+beefInput300.vegetation = beefInputData.vegetation;
 
 export const calculateBeef300 = () => calculateBeef(beefInput300);
--- examples/ts-client/src/index.ts
+++ examples/ts-client/src/index.ts
@@ -1,5 +1,3 @@
-import { calculateBeefLatest } from './beef/latest';
 import { calculateBeef300 } from './beef/v300';
 
-calculateBeefLatest();
 calculateBeef300();
