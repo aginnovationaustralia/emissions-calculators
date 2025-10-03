323f7320f1f7b97890bfeb88daa0f27a89c32b39
Eddie Sholl <eddie.sholl@exogee.com>
2025-10-03 11:59:09 +1000

    Fix EnvironmentContext export

--- packages/calculators/src/versions/3.0.0/constants/index.ts
+++ packages/calculators/src/versions/3.0.0/constants/index.ts
@@ -1,6 +1,5 @@
-import { CalculationEnvironment } from '../execution/environment';
 import { constants } from './constant_values';
 import { loadConstants } from './constantsLoader';
 import { Constants } from './versionedConstants';
 
-export { CalculationEnvironment, constants, Constants, loadConstants };
+export { constants, Constants, loadConstants };
