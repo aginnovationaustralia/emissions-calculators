27bb8192944ce1e7419bf9d57f284906cd95830c
Eddie Sholl <eddie.sholl@exogee.com>
2025-11-07 10:24:07 +1100

    Adding a little more context when an emissions test fails, to make it easier to track down the cause

--- packages/calculators/src/versions/3.0.0/test/common/emissions.ts
+++ packages/calculators/src/versions/3.0.0/test/common/emissions.ts
@@ -22,31 +22,40 @@ const recurseExpectations = (
   path: string[] = [],
 ) => {
   Object.entries(expectations).forEach(([k1, v1]) => {
-    if (typeof v1 === 'number') {
-      if (emissions === undefined) {
-        console.log(path, k1, emissions);
+    try {
+      if (typeof v1 === 'number') {
+        if (emissions === undefined) {
+          console.log(path, k1, emissions);
+        }
+        executeKeyValues([...path, k1], v1 as number, emissions[k1] as number);
+      } else if (Array.isArray(v1)) {
+        if (typeof v1[0] === 'number') {
+          (v1 as number[]).forEach((v2, i) => {
+            executeKeyValues([...path, k1], v2, (emissions[k1] as number[])[i]);
+          });
+        } else if (typeof v1[0] === 'object') {
+          (v1 as KeyValuePairs[]).forEach((v2, i) => {
+            recurseExpectations(v2, (emissions[k1] as KeyValuePairs[])[i], [
+              ...path,
+              k1,
+            ]);
+          });
+        }
+      } else if (typeof v1 === 'object') {
+        recurseExpectations(
+          v1 as KeyValuePairs,
+          emissions[k1] as KeyValuePairs,
+          [...path, k1],
+        );
+      } else if (typeof v1 !== 'string') {
+        throw new Error(`Invalid expectations ${typeof v1} for ${k1}`);
       }
-      executeKeyValues([...path, k1], v1 as number, emissions[k1] as number);
-    } else if (Array.isArray(v1)) {
-      if (typeof v1[0] === 'number') {
-        (v1 as number[]).forEach((v2, i) => {
-          executeKeyValues([...path, k1], v2, (emissions[k1] as number[])[i]);
-        });
-      } else if (typeof v1[0] === 'object') {
-        (v1 as KeyValuePairs[]).forEach((v2, i) => {
-          recurseExpectations(v2, (emissions[k1] as KeyValuePairs[])[i], [
-            ...path,
-            k1,
-          ]);
-        });
-      }
-    } else if (typeof v1 === 'object') {
-      recurseExpectations(v1 as KeyValuePairs, emissions[k1] as KeyValuePairs, [
-        ...path,
-        k1,
-      ]);
-    } else if (typeof v1 !== 'string') {
-      throw new Error(`Invalid expectations ${typeof v1} for ${k1}`);
+    } catch (error) {
+      throw new Error(
+        `Error in recurseExpectations for key ${k1} at path '${path.join(
+          ' -> ',
+        )}': ${error}`,
+      );
     }
   });
 };
