66253a5bbd6dca779111fdf360e741c232e7cdd0
Eddie Sholl <eddie.sholl@exogee.com>
2025-12-02 11:22:50 +1100

    Add useful comment

--- packages/calculators/src/calculators/index.ts
+++ packages/calculators/src/calculators/index.ts
@@ -27,6 +27,30 @@ export { CalculatorOptions } from './execution/types';
 export { CalculatorNames, isValidCalculatorName } from './strings';
 export { validateCalculatorInput } from './validate';
 
+/**
+ * Calculates the emissions for a given calculator name and input. It will perform validation of the input
+ * before passing it to the calculator function, based on the calculator name you provide.
+ * The type of the output is determined by the calculator name you provide.
+ *
+ * const result = calculateEmissions('beef', userInputData, { disableMetrics: true });
+ * if (result.succeeded) {
+ *   const emissions: BeefInput = result.emissions;
+ *   console.log('Emissions calculated correctly', emissions);
+ * } else if (result.valid) {
+ *   console.error('Emissions calculation failed', result.error);
+ * } else {
+ *   const inputValidationError: InputValidationError = result.error;
+ *   console.error('Input was not valid', inputValidationError.errors);
+ * }
+ *
+ * return result;
+ *
+ * @param calculatorName - The name of the calculator to use
+ * @param input - The input to the calculator
+ * @param options - The options to the calculator
+ * @returns CalculateEmissionsResult<O>
+ * @function calculateEmissions
+ */
 export const calculateEmissions = generateCalculateEmissions({
   calculateBeef,
   calculateAquaculture,
