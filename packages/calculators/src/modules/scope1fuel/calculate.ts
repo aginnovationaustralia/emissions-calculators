03fe67bd53672c3adc55423250112c0c165220e3
Eddie Sholl <eddie.sholl@exogee.com>
2026-01-25 16:25:33 +1100

    Tidy functions for transport fuel

--- packages/calculators/src/modules/scope1fuel/calculate.ts
+++ packages/calculators/src/modules/scope1fuel/calculate.ts
@@ -39,9 +39,36 @@ const emissionsOfGasForFuel = <GasType extends 'CO2' | 'CH4' | 'N2O'>(
   );
   const energyFromFuel = multiply(energyContentFactorForFuel, amountOfFuel);
 
-  const gasEmissionsFromFuel = multiply(efForFuel, energyFromFuel);
+  return multiply(efForFuel, energyFromFuel);
+};
 
-  return gasEmissionsFromFuel;
+const transportEmissionsForGas = <GasType extends 'CO2' | 'CH4' | 'N2O'>(
+  dieselUse: TypedOrigin<Volume<'Fuel'>>,
+  petrolUse: TypedOrigin<Volume<'Fuel'>>,
+  lpgUse: TypedOrigin<Volume<'Fuel'>>,
+  constants: ConstantsForGrainsCalculator,
+  gasType: GasType,
+) => {
+  const dieselEmissions = emissionsOfGasForFuel(
+    dieselUse,
+    constants,
+    'DIESEL',
+    gasType,
+  );
+  const petrolEmissions = emissionsOfGasForFuel(
+    petrolUse,
+    constants,
+    'PETROL',
+    gasType,
+  );
+  const lpgEmissions = emissionsOfGasForFuel(lpgUse, constants, 'LPG', gasType);
+  return sum(
+    {
+      items: [dieselEmissions, petrolEmissions, lpgEmissions],
+      unit: mass(gasType),
+    },
+    { name: `EtransGHG${gasType}`, valueType: 'variable', unit: mass(gasType) },
+  );
 };
 
 // 6.1 Transport fuel
@@ -53,83 +80,30 @@ export const calculateScope1Fuel = (
 ) => {
   const { constants } = context;
 
-  //   const zeroCO2 = rootOrigin(mass('CO2', new Decimal(0)), {
-  //     name: 'zero',
-  //     valueType: 'constant',
-  //   });
-  //   const zeroCH4 = rootOrigin(mass('CH4', new Decimal(0)), {
-  //     name: 'zero',
-  //     valueType: 'constant',
-  //   });
-  //   const zeroN2O = rootOrigin(mass('N2O', new Decimal(0)), {
-  //     name: 'zero',
-  //     valueType: 'constant',
-  //   });
-
-  const dieselCO2 = emissionsOfGasForFuel(
-    crop.dieselUse,
-    constants,
-    'DIESEL',
-    'CO2',
-  );
-  const dieselCH4 = emissionsOfGasForFuel(
-    crop.dieselUse,
-    constants,
-    'DIESEL',
-    'CH4',
-  );
-  const dieselN2O = emissionsOfGasForFuel(
-    crop.dieselUse,
-    constants,
-    'DIESEL',
-    'N2O',
-  );
-  const petrolCO2 = emissionsOfGasForFuel(
-    crop.petrolUse,
-    constants,
-    'PETROL',
-    'CO2',
-  );
-  const petrolCH4 = emissionsOfGasForFuel(
-    crop.petrolUse,
-    constants,
-    'PETROL',
-    'CH4',
-  );
-  const petrolN2O = emissionsOfGasForFuel(
-    crop.petrolUse,
-    constants,
-    'PETROL',
-    'N2O',
-  );
-  const lpgCO2 = emissionsOfGasForFuel(crop.lpg, constants, 'LPG', 'CO2');
-  const lpgCH4 = emissionsOfGasForFuel(crop.lpg, constants, 'LPG', 'CH4');
-  const lpgN2O = emissionsOfGasForFuel(crop.lpg, constants, 'LPG', 'N2O');
-
   // 6.1.1
   // Line 55
-  const fuelCO2: SummedOrigin<Mass<'CO2'>> = sum(
-    {
-      items: [dieselCO2, petrolCO2, lpgCO2],
-      unit: mass('CO2'),
-    },
-    { name: 'EtransGHGCO2', valueType: 'variable', unit: mass('CO2') },
+  const fuelCO2: SummedOrigin<Mass<'CO2'>> = transportEmissionsForGas(
+    crop.dieselUse,
+    crop.petrolUse,
+    crop.lpg,
+    constants,
+    'CO2',
   );
 
-  const fuelCH4: SummedOrigin<Mass<'CH4'>> = sum(
-    {
-      items: [dieselCH4, petrolCH4, lpgCH4],
-      unit: mass('CH4'),
-    },
-    { name: 'EtransGHGCH4', valueType: 'variable', unit: mass('CH4') },
+  const fuelCH4: SummedOrigin<Mass<'CH4'>> = transportEmissionsForGas(
+    crop.dieselUse,
+    crop.petrolUse,
+    crop.lpg,
+    constants,
+    'CH4',
   );
 
-  const fuelN2O: SummedOrigin<Mass<'N2O'>> = sum(
-    {
-      items: [dieselN2O, petrolN2O, lpgN2O],
-      unit: mass('N2O'),
-    },
-    { name: 'EtransGHGN2O', valueType: 'variable', unit: mass('N2O') },
+  const fuelN2O: SummedOrigin<Mass<'N2O'>> = transportEmissionsForGas(
+    crop.dieselUse,
+    crop.petrolUse,
+    crop.lpg,
+    constants,
+    'N2O',
   );
   return {
     fuelCO2,
